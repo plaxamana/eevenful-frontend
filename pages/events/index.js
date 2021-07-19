555f513116ab62606203fe7588981492b1c78083
Philip Laxamana <philip.laxamana@hotmail.com>
2021-07-18 20:46:08 -0400

    listing our events using getServerSideProps

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,26 @@
-import Layout from "@/components/Layout";
+import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+import { API_URL } from '@/config/index'
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
   return (
     <Layout>
-      <h1>My Events</h1>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>no events to show</h3>}
+      {events.map(event => (
+        <EventItem key={event.id} event={event} />
+      ))}
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: {
+      events,
+    },
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
 import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
 import { API_URL } from '@/config/index'
+import Link from 'next/link'
 
 export default function Home({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
-      <ul>
-        {events.map(event => (
-          <li key={event.id}>{event.name}</li>
-        ))}
-      </ul>
+      {events.length === 0 && <h3>no events to show</h3>}
+      {events.map(event => (
+        <EventItem key={event.id} event={event} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className='btn-secondary'>View All Events</a>
+        </Link>
+      )}
     </Layout>
   )
 }
@@ -20,8 +27,7 @@ export async function getServerSideProps() {
 
   return {
     props: {
-      events,
+      events: events.slice(0, 3),
     },
-    revalidate: 1,
   }
 }
