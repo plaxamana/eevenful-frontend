88e4675dc2a27e34c4b970e7b32370d11252b1f5
Philip Laxamana <philip.laxamana@hotmail.com>
2021-07-18 20:29:51 -0400

    fetching data from our API route

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-export default function Home() {
+export default function Home({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
+      <ul>
+        {events.map(event => (
+          <li key={event.id}>{event.name}</li>
+        ))}
+      </ul>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: {
+      events,
+    },
+    revalidate: 1,
+  }
 }
