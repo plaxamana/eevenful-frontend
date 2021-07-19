5eddd7711a3c504aa9477525dd1fdb8b8f9d245c
Philip Laxamana <philip.laxamana@hotmail.com>
2021-07-18 23:45:40 -0400

    implemented search

--- components/Search.js
+++ components/Search.js
@@ -1,7 +1,27 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import styles from '@/styles/Search.module.css'
+
 export default function Search() {
+  const [term, setTerm] = useState('')
+  const router = useRouter()
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    router.push(`/events/search?term=${term}`)
+    setTerm('')
+  }
+
   return (
-    <div>
-      
+    <div className={styles.search}>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={term}
+          onChange={e => setTerm(e.target.value)}
+          placeholder='Search Events'
+        />
+      </form>
     </div>
   )
 }
